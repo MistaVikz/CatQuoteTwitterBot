5506591ee62b4414e24cfae65663566f325acdd9
Mark <markdvickers@icloud.com>
2016-09-04 20:08:58 -0600

    feat: Finished Basic Tweetbot Functionality

---
+++ README.md
@@ -0,0 +1,1 @@
+# CatQuoteTwitterBot
---
+++ emmalikestomeow.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import sys
+import os
+import random
+from twython import Twython
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+    )
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+def tweetQuote():
+    api = Twython (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+    )
+
+    try:
+        quoteFile = open(os.path.join(__location__,'quotes.txt'),'r')
+        quoteList = quoteFile.readlines()
+        quoteFile.close()
+
+        rChoice = random.randrange(len(quoteList))
+        rTweet = random.randrange(3)
+        
+        if rTweet == 0:
+            api.update_status(status=quoteList[rChoice] + "#cats #CatsOfTwitter")
+            return None
+        else:
+            return None
+    except IOError:
+        return None
+        
+if __name__ == "__main__":
+    tweetQuote()
